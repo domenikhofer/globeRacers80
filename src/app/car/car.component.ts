f24831d6ca9bad08f0094ab1b8431874cfc3e9a3
Vanessa Oppowa <vanessa.oppowa87@gmail.com>
2017-10-20 18:41:42 +0200

    start button, tacho layout (not finished)

--- src/app/car/car.component.ts
+++ src/app/car/car.component.ts
@@ -1,22 +1,42 @@
 import {Component, Input, EventEmitter, OnInit, Output, OnChanges} from '@angular/core';
+import {animate, keyframes, query, stagger, state, style, transition, trigger} from '@angular/animations';
 import  anime from 'animejs';
+
 
 @Component({
   selector: 'app-car',
   templateUrl: './car.component.html',
   styleUrls: ['./car.component.scss'],
+  animations: [
+    trigger('carTrigger', [
+      transition('void => *', [
+        style({transform: 'translateY(-100%)' }),
+        animate('1s')
+      ])
+    ]),
+    trigger('startContainer', [
+      transition('void => *', [
+        style({transform: 'translateY(-100%)' }),
+        animate('1s')
+      ])
+    ])
+  ]
 })
 
 export class CarComponent implements OnInit, OnChanges {
   @Input() distance;
   @Input() ms;
   @Output() carClicked: EventEmitter<any> = new EventEmitter();
-
+  showCar: boolean = false;
 
   constructor() {}
 
   ngOnInit() {
     this.ms = 0;
+  }
+
+  togglesstates() {
+    this.showCar = true;
   }
 
   ngOnChanges(changes: any) {
