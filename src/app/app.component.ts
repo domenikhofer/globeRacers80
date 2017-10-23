a91d906dfe9cc4bc4e7b449eb982085e4066c6e6
Vanessa Oppowa <vanessa.oppowa87@gmail.com>
2017-10-23 17:35:19 +0200

    added small logo animation onInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import anime from 'animejs';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,6 +12,11 @@ export class AppComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    anime({
+      targets: '.logo',
+      rotate: '1turn',
+      duration: 5000,
+      delay: 1000,
+    });
   }
-
 }
