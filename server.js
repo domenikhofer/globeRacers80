8edbf92a935bd4b9f5414b742acebab466102e5f
Vanessa Oppowa <vanessa.oppowa87@gmail.com>
2017-12-18 17:00:18 +0100

    new server

--- server.js
+++ server.js
@@ -22,12 +22,12 @@ app.use(bodyParser.urlencoded({
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-/*
+
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
-});*/
+});
 
 
 //////DEV/////////////
