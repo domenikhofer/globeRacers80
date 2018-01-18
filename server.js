fcd91fec038cb5f7d3ede10df8824af63a2e5987
Domenik Hofer <domenik.hofer@swisscom.com>
2018-01-18 15:15:06 +0100

    heroku deploy 10

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://globeracers.herokuapp.com/");
+  res.header("Access-Control-Allow-Origin", "https://globeracers.herokuapp.com");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  server: 'https://globeracers.herokuapp.com/'
+  server: 'https://globeracers.herokuapp.com'
 };
