113b26da3b061503f2fe0820bb04abcad9aa2bbb
Domenik Hofer <domenik.hofer@swisscom.com>
2018-01-19 11:37:29 +0100

    heroku deploy 29

--- src/app/achievement/achievement.component.ts
+++ src/app/achievement/achievement.component.ts
@@ -64,7 +64,7 @@ export class AchievementComponent implements OnInit, OnChanges {
         userData.data.achievements.map(
           x => this.allAchievements.find(
             y => y.id === x).title
-        );
+        ).reverse();
     }
   }
 
